8f423e2a4f5c98d563b6f611cba741f60b41e284
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2025-02-04 18:54:31 +0100

    get current user data from UserButton and improve its styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
 "use client";
 import UserButton from "@/features/auth/components/user-button";
-import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
-  return (
-    <>
-      <p>
-        Logged In!
-        <UserButton />
-      </p>
-    </>
-  );
+  return <UserButton />;
 }
