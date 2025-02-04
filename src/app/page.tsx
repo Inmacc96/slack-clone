295ea22d1e1ae2109c27b30b2a7d04220c5c3eda
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2025-02-04 18:06:02 +0100

    create userButton component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+import UserButton from "@/features/auth/components/user-button";
+import { useAuthActions } from "@convex-dev/auth/react";
+
 export default function Home() {
-  return <p>Home</p>;
+  return (
+    <>
+      <p>
+        Logged In!
+        <UserButton />
+      </p>
+    </>
+  );
 }
