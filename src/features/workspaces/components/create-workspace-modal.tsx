f5b9c65237e5a9e96dab2ea39910033564cd11b2
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2025-02-06 19:59:03 +0100

    layout createWorkspace modal

--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -2,13 +2,14 @@
 import {
   Dialog,
   DialogContent,
+  DialogTitle,
   DialogDescription,
   DialogHeader,
   DialogFooter,
 } from "@/components/ui/dialog";
-
+import { Input } from "@/components/ui/input";
 import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
-import { DialogTitle } from "@radix-ui/react-dialog";
+import { Button } from "@/components/ui/button";
 
 const CreateWorkspaceModal = () => {
   const [isOpen, setIsOpen] = useCreateWorkspaceModal();
@@ -24,6 +25,19 @@ const CreateWorkspaceModal = () => {
         <DialogHeader>
           <DialogTitle>Add a workspace</DialogTitle>
         </DialogHeader>
+        <form className="space-y-4">
+          <Input
+            value=""
+            disabled={false}
+            required
+            autoFocus
+            minLength={3}
+            placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+          />
+          <div className="flex justify-end">
+            <Button disabled={false}>Create</Button>
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
