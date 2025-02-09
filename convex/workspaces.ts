b6463d392802244ea7b44df22da6de3ef54fba48
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2025-02-09 13:44:40 +0100

    update create workspace mutation: create admin member after workspace

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,7 +1,6 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
-import { Id } from "./_generated/dataModel";
 
 export const create = mutation({
   args: { name: v.string() },
@@ -12,7 +11,15 @@ export const create = mutation({
     // TODO: Create a proper method later
     const joinCode = "123456";
 
-    return await ctx.db.insert("workspaces", { name, userId, joinCode });
+    const workspaceId = await ctx.db.insert("workspaces", {
+      name,
+      userId,
+      joinCode,
+    });
+
+    await ctx.db.insert("members", { userId, workspaceId, role: "admin" });
+
+    return workspaceId;
   },
 });
 
