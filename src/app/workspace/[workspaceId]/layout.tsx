37d618aef6bab33f5057cd440163e32407476a0c
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2025-02-10 18:15:51 +0100

    start to layout workspace sidebar: layout worskpace header

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,6 +1,12 @@
 "use client";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 import Sidebar from "./sidebar";
 import Toolbar from "./toolbar";
+import WorkspaceSidebar from "./workspace-sidebar";
 
 interface WorkspaceLayoutProps {
   children: React.ReactNode;
@@ -12,7 +18,20 @@ const WorkspaceLayout: React.FC<WorkspaceLayoutProps> = ({ children }) => {
       <Toolbar />
       <div className="flex h-[calc(100vh-40px)]">
         <Sidebar />
-        {children}
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="workspace-layout"
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={12}
+            className="bg-[#5e3c5f]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </div>
   );
