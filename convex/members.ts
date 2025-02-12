a1ee6fed6145cf485916d362c66390009a2fd76f
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2025-02-12 19:22:42 +0100

    implement functionality to get members

--- convex/members.ts
+++ convex/members.ts
@@ -1,6 +1,41 @@
 import { v } from "convex/values";
-import { query } from "./_generated/server";
+import { query, QueryCtx } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
+import { Id } from "./_generated/dataModel";
+
+const populateUser = (ctx: QueryCtx, id: Id<"users">) => {
+  return ctx.db.get(id);
+};
+
+export const get = query({
+  args: { workspaceId: v.id("workspaces") },
+  handler: async (ctx, { workspaceId }) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) return [];
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", workspaceId).eq("userId", userId)
+      )
+      .unique();
+    if (!member) return [];
+
+    const data = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id", (q) => q.eq("workspaceId", workspaceId))
+      .collect();
+
+    const members = await Promise.all(
+      data.map(async (member) => {
+        const user = await populateUser(ctx, member.userId);
+        return user ? { ...member, user } : null;
+      })
+    );
+
+    return members.filter((member) => member !== null);
+  },
+});
 
 export const current = query({
   args: {
@@ -9,12 +44,12 @@ export const current = query({
   handler: async (ctx, { workspaceId }) => {
     const userId = await getAuthUserId(ctx);
     if (!userId) return null;
-    const member = await ctx.db
+
+    return await ctx.db
       .query("members")
       .withIndex("by_workspace_id_user_id", (q) =>
         q.eq("workspaceId", workspaceId).eq("userId", userId)
       )
       .unique();
-    return member ?? null;
   },
 });
