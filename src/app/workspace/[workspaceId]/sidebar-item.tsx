e841636f19d008ccec3c2f688b7c29099bc77055
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2025-02-12 17:12:34 +0100

    create SidebarItem component

---
+++ src/app/workspace/[workspaceId]/sidebar-item.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import { LucideIcon } from "lucide-react";
+import { IconType } from "react-icons/lib";
+import { cva, type VariantProps } from "class-variance-authority";
+import { Button } from "@/components/ui/button";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { cn } from "@/lib/utils";
+
+const sidebarItemVariants = cva(
+  "flex items-center gap-1.5 justify-start font-normal h-7 px-4 text-sm overflow-hidden",
+  {
+    variants: {
+      variant: {
+        default: "text-[#f9edffcc]",
+        active: "text-[#481349] bg-white/90 hover:bg-white/90",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+interface SidebarItemProps {
+  label: string;
+  id: string;
+  icon: LucideIcon | IconType;
+  variant?: VariantProps<typeof sidebarItemVariants>["variant"];
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+  label,
+  id,
+  icon: Icon,
+  variant,
+}) => {
+  const workspaceId = useWorkspaceId();
+
+  return (
+    <Button
+      className={cn(sidebarItemVariants({ variant }))}
+      variant="transparent"
+      size="sm"
+      asChild
+    >
+      <Link href={`/workspace/${workspaceId}/channel/${id}`}>
+        <Icon className="size-3.5 mr-1 shrink-0" />
+        <span className="text-sm truncate">{label}</span>
+      </Link>
+    </Button>
+  );
+};
+
+export default SidebarItem;
