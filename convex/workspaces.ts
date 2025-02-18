d32f79b76cbbed0923ca5ed28c04e8d73daffdf6
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2025-02-18 19:38:48 +0100

    implement functionality to generate new join code for only admin member

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -9,6 +9,26 @@ const generateCode = () => {
   ).join("");
 };
 
+export const newJoinCode = mutation({
+  args: { workspaceId: v.id("workspaces") },
+  handler: async (ctx, { workspaceId }) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) throw new Error("Unauthorized");
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", workspaceId).eq("userId", userId)
+      )
+      .unique();
+    if (!member || member.role !== "admin") throw new Error("Unauthorized");
+
+    await ctx.db.patch(workspaceId, { joinCode: generateCode() });
+
+    return workspaceId;
+  },
+});
+
 export const create = mutation({
   args: { name: v.string() },
   handler: async (ctx, { name }) => {
--- src/app/workspace/[workspaceId]/invite-modal.tsx
+++ src/app/workspace/[workspaceId]/invite-modal.tsx
@@ -1,4 +1,5 @@
-import { CopyIcon } from "lucide-react";
+import { toast } from "sonner";
+import { CopyIcon, RefreshCcw } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -6,12 +7,11 @@ import {
   DialogTitle,
   DialogHeader,
   DialogDescription,
-  DialogFooter,
   DialogClose,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { toast } from "sonner";
+import { useNewJoinCode } from "@/features/workspaces/api/use-new-join-code";
+import { useConfirm } from "@/hooks/use-confirm";
 
 interface InviteModalProps {
   open: boolean;
@@ -27,6 +27,28 @@ const InviteModal: React.FC<InviteModalProps> = ({
   joinCode,
 }) => {
   const workspaceId = useWorkspaceId();
+  const { mutate, isPending } = useNewJoinCode();
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "This will deactivate the current invite code and generate a new one."
+  );
+
+  const handleNewCode = async () => {
+    const ok = await confirm();
+    if (!ok) return;
+
+    mutate(
+      { workspaceId },
+      {
+        onSuccess: () => {
+          toast.success("Invite code regenerated");
+        },
+        onError: () => {
+          toast.error("Failed to regenerate invite code");
+        },
+      }
+    );
+  };
 
   const handleCopy = () => {
     const inviteLink = `${window.location.origin}/join/${workspaceId}`;
@@ -37,24 +59,39 @@ const InviteModal: React.FC<InviteModalProps> = ({
   };
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Invite people to {name}</DialogTitle>
-          <DialogDescription>
-            Use the code below to invite people to your workspace
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex flex-col gap-y-4 items-center justify-center py-10">
-          <p className="text-4xl font-bold tracking-widest uppercase">
-            {joinCode}
-          </p>
-          <Button variant="ghost" size="sm" onClick={handleCopy}>
-            Copy link <CopyIcon className="size-4 ml-2" />
-          </Button>
-        </div>
-      </DialogContent>
-    </Dialog>
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Invite people to {name}</DialogTitle>
+            <DialogDescription>
+              Use the code below to invite people to your workspace
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col gap-y-4 items-center justify-center py-10">
+            <p className="text-4xl font-bold tracking-widest uppercase">
+              {joinCode}
+            </p>
+            <Button variant="ghost" size="sm" onClick={handleCopy}>
+              Copy link <CopyIcon className="size-4" />
+            </Button>
+          </div>
+          <div className="flex items-center justify-between w-full">
+            <Button
+              disabled={isPending}
+              variant="outline"
+              onClick={handleNewCode}
+            >
+              New code <RefreshCcw className="size-4" />
+            </Button>
+            <DialogClose asChild>
+              <Button>Close</Button>
+            </DialogClose>
+          </div>
+        </DialogContent>
+      </Dialog>
+      <ConfirmDialog />
+    </>
   );
 };
 
