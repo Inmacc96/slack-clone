6627e75612cd45c84e117ba08efa35d9853dd911
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2025-02-22 17:18:30 +0100

    layout header of channel page

---
+++ src/app/workspace/[workspaceId]/channel/[channelId]/header.tsx
@@ -0,0 +1,55 @@
+import { FaChevronDown } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogHeader,
+  DialogTitle,
+  DialogContent,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { TrashIcon } from "lucide-react";
+
+interface HeaderProps {
+  title: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ title }) => {
+  return (
+    <div className="bg-white border-b h-12 flex items-center px-4 overflow-hidden">
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            variant="ghost"
+            className="text-lg font-semibold px-2 overflow-hidden w-auto"
+            size="sm"
+          >
+            <span className="truncate"># {title}</span>
+            <FaChevronDown className="size-2.5" />
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="p-0 bg-gray-50 overflow-hidden">
+          <DialogHeader className="p-4 border-b bg-white">
+            <DialogTitle># {title}</DialogTitle>
+          </DialogHeader>
+          <div className="px-4 pb-4 flex flex-col gap-y-2">
+            <div className="px-5 py-4 bg-white rounded-lg border cursror-pointer hover:bg-gray-50">
+              <div className="flex items-center justify-between">
+                <p className="text-sm font-semibold">Channel name</p>
+                <p className="text-sm text-[#1264a3] hover:underline font-semibold">
+                  Edit
+                </p>
+              </div>
+              <p className="text-sm"># {title}</p>
+            </div>
+            <button className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg cursor-pointer border hover:bg-gray-50 text-rose-600">
+              <TrashIcon className="size-4" />
+              <p className="text-sm font-semibold">Delete Channel</p>
+            </button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default Header;
--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,5 +1,37 @@
+"use client";
+import { Loader, TriangleAlert } from "lucide-react";
+import { useGetChannel } from "@/features/channels/api/use-get-channel";
+import { useChannelId } from "@/hooks/use-channel-id";
+import Header from "./header";
+
 const ChannelIdPage = () => {
-  return <div>Channel Id pAGE</div>;
+  const channelId = useChannelId();
+  const { data: channel, isLoading: channelLoading } = useGetChannel({
+    id: channelId,
+  });
+
+  if (channelLoading) {
+    return (
+      <div className="flex flex-1 h-full items-center justify-center">
+        <Loader className="size-5 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+
+  if (!channel) {
+    return (
+      <div className="flex flex-1 h-full items-center justify-center flex-col gap-2">
+        <TriangleAlert className="size-6 text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">Channel not found</span>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col h-full">
+      <Header title={channel.name} />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
