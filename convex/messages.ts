1527d19a8aad69f39a9318f1ccb40c24b33e5eea
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2025-03-06 20:09:41 +0100

    define getMessages API endpoint

--- convex/messages.ts
+++ convex/messages.ts
@@ -1,7 +1,52 @@
 import { v } from "convex/values";
-import { mutation, QueryCtx } from "./_generated/server";
+import { mutation, query, QueryCtx } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
 import { Id } from "./_generated/dataModel";
+import { paginationOptsValidator } from "convex/server";
+const populateReactions = (ctx: QueryCtx, messageId: Id<"messages">) => {
+  return ctx.db
+    .query("reactions")
+    .withIndex("by_message_id", (q) => q.eq("messageId", messageId))
+    .collect();
+};
+
+const populateUser = (ctx: QueryCtx, userId: Id<"users">) => {
+  return ctx.db.get(userId);
+};
+
+const populateMember = (ctx: QueryCtx, memberId: Id<"members">) => {
+  return ctx.db.get(memberId);
+};
+
+const populateThread = async (ctx: QueryCtx, messageId: Id<"messages">) => {
+  const messages = await ctx.db
+    .query("messages")
+    .withIndex("by_parent_message_id", (q) =>
+      q.eq("parentMessageId", messageId)
+    )
+    .collect();
+  if (messages.length === 0) {
+    return { count: 0, images: undefined, timestamp: 0 };
+  }
+
+  const lastMessage = messages[messages.length - 1];
+  const lastMessageMember = await populateMember(ctx, lastMessage.memberId);
+  if (!lastMessageMember) {
+    return {
+      count: 0,
+      image: undefined,
+      timestamp: 0,
+    };
+  }
+
+  const lastMessageUser = await populateUser(ctx, lastMessageMember.userId);
+
+  return {
+    count: messages.length,
+    image: lastMessageUser?.image,
+    timestamp: lastMessage._creationTime,
+  };
+};
 
 const getMember = async (
   ctx: QueryCtx,
@@ -15,6 +60,45 @@ const getMember = async (
     )
     .unique();
 };
+
+export const get = query({
+  args: {
+    channelId: v.optional(v.id("channels")),
+    conversationId: v.optional(v.id("conversations")),
+    parentMessageId: v.optional(v.id("messages")),
+    paginationOpts: paginationOptsValidator,
+  },
+  handler: async (
+    ctx,
+    { channelId, conversationId, parentMessageId, paginationOpts }
+  ) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) throw new Error("Unauthorized");
+
+    let _conversationId = conversationId;
+
+    // Only possible if we are replying in a thread in 1:1 conversation
+    if (!conversationId && !channelId && parentMessageId) {
+      const parentMessage = await ctx.db.get(parentMessageId);
+      if (!parentMessage) throw new Error("Parent message not fount");
+
+      _conversationId = parentMessage.conversationId;
+    }
+
+    const results = await ctx.db
+      .query("messages")
+      .withIndex("by_channel_id_parent_message_id_conversation_id", (q) =>
+        q
+          .eq("channelId", channelId)
+          .eq("parentMessageId", parentMessageId)
+          .eq("conversationId", _conversationId)
+      )
+      .order("desc")
+      .paginate(paginationOpts);
+
+    return results;
+  },
+});
 
 export const create = mutation({
   args: {
